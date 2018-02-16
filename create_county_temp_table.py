9023552e5e5978639210733233c50ce065b276de
ginaliqili <ginali@vt.edu>
2018-02-16 15:55:29 -0800

    script

--- create_county_temp_table.py
+++ create_county_temp_table.py
@@ -23,20 +23,22 @@ for county_data in county_data:
     # Find all stations contained in given county
     cur.execute("SELECT a.station_id from " + station_location_table + " as a right join " + us_counties_table + " as b on st_contains(b.geom, a.geom) where b.geoid='" + county_data[0] + "'")
     stations_contained = cur.fetchall()
-    for station_contained in stations_contained:
-        # Find all Tmax and Tmin readings from that station and add (statefp, county, obs_type, value, date) to new table
-        cur.execute("SELECT obs_type, value, date FROM " + sys.argv[3] + " WHERE station_id = '" + station_contained[0] + "'")
-        result = cur.fetchall()
-        if len(result) != 0:
-            print("result: " + str(len(result)))
-            print("output table: " + sys.argv[3])
-            print("station_id: " + station_contained[0])
-            print("statefp: " + county_data[1])
-            print("county: " + county_data[2])
-            for r in result:
-                print(str(r))
-                cur.execute("INSERT INTO " + sys.argv[4] + " (statefp, county, obs_type, value, date) VALUES (" + county_data[1] + ", '" + county_data[2] + "', '" + r[0] + "', " + str(r[1]) + ", '" + str(r[2]) + "')")
-                conn.commit()
+    # If the county has stations inside of it:
+    if len(stations_contained) != 0:
+        for station_contained in stations_contained:
+            # Find all Tmax and Tmin readings from that station and add (statefp, county, obs_type, value, date) to new table
+            cur.execute("SELECT obs_type, value, date FROM " + sys.argv[3] + " WHERE station_id = '" + station_contained[0] + "'")
+            result = cur.fetchall()
+            if len(result) != 0:
+                print("result: " + str(len(result)))
+                print("output table: " + sys.argv[3])
+                print("station_id: " + station_contained[0])
+                print("statefp: " + county_data[1])
+                print("county: " + county_data[2])
+                for r in result:
+                    print(str(r))
+                    cur.execute("INSERT INTO " + sys.argv[4] + " (statefp, county, obs_type, value, date) VALUES (" + county_data[1] + ", '" + county_data[2] + "', '" + r[0] + "', " + str(r[1]) + ", '" + str(r[2]) + "')")
+                    conn.commit()
 cur.close()
 conn.close()
 
